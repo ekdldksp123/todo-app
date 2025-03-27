56b4598643988f6acf0d7b18c46957d0f946e9c5
ekdldksp123 <ekdldksp123@naver.com>
2025-03-27 22:42:03 +0900

    :sparkles: add filterTodos feature

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,15 +1,32 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export function cn(...inputs: ClassValue[]) {
+export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs))
 }
 
-export function formatDate(milliseconds: number) {
+export const formatDate = (milliseconds: number) => {
   const date = new Date(milliseconds)
   const year = date.getFullYear()
-  const month = String(date.getMonth() + 1).padStart(2, '0') // 월은 0부터 시작하므로 +1
+  const month = String(date.getMonth() + 1).padStart(2, '0') // month는 0부터 시작하므로 +1
   const day = String(date.getDate()).padStart(2, '0')
 
   return `${year}-${month}-${day}`
+}
+
+export const parseDate = (dateString: string): Date => {
+  const [year, month, day] = dateString.split('-').map(Number)
+  return new Date(year, month - 1, day) // month는 0부터 시작
+}
+
+export const isWithinThreeDays = (deadline: number): boolean => {
+  const deadlineDate = new Date(deadline)
+  const now = new Date()
+  const threeDaysLater = new Date(now.getTime() + 3 * 24 * 60 * 60 * 1000)
+
+  return deadlineDate <= threeDaysLater
+}
+
+export const isInDeadline = (date: Date, deadline: Date): boolean => {
+  return date <= deadline
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,26 +3,42 @@ import { FC, useEffect, useLayoutEffect } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import { getAllTodos } from '../../api'
 import { AddTodoForm, TaskList } from '../../components/pages/Home'
+import { isInDeadline, isWithinThreeDays, parseDate } from '../../libs/utils'
 
 const Home: FC = () => {
   const [searchParams] = useSearchParams()
 
-  // const filterTodos = async () => {
-  //   const todos = await getAllTodos()
-  //   if(searchParams) {
-  //     let filteredList = [...todos];
-  //     searchParams.forEach((value, key) => {
-  //       if(key === 'status') {
-  //         filteredList = filteredList.filter(({done}) =>)
-  //       }
-  //     })
-  //   }
-  //   return todos;
-  // }
+  const filterTodos = async () => {
+    const todos = (await getAllTodos()) ?? []
+    if (!todos.length) return todos
+
+    if (searchParams) {
+      const { keyword, status, period } = searchParams
+      return todos.filter((todo) => {
+        // 키워드 필터
+        const matchesKeyword = !keyword ? true : todo.text.includes(keyword)
+
+        let matchesStatus = true
+        if (status === 'done') {
+          matchesStatus = todo.done === true
+        } else if (status === 'imminent') {
+          matchesStatus = isWithinThreeDays(period)
+        } else if (status === 'todo') {
+          matchesStatus = todo.done === false
+        }
+
+        const matchesPeriod = !period
+          ? true
+          : isInDeadline(parseDate(period), new Date(todo.deadline))
+        return matchesKeyword && matchesStatus && matchesPeriod
+      })
+    }
+    return todos
+  }
 
   const { data, isLoading, isError, refetch } = useQuery({
     queryKey: ['todos', searchParams],
-    queryFn: getAllTodos,
+    queryFn: filterTodos,
   })
 
   useEffect(() => {
